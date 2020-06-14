809b981c9a5e083d026820d883214cccf4d2941e
mina bessada <48674093+mina-adibe@users.noreply.github.com>
2020-06-14 14:58:08 +0200

    handle navigation menu by event listener

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+/* -------------------------- handle scrolling  ------------------------*/
+
 const sections = [].slice.call(document.querySelectorAll("section"));
 
 function createObserver(el) {
@@ -33,3 +35,43 @@ const setup = (sections) => {
 };
 
 setup(sections);
+
+/* -------------------------- handle navigation menu------------------------*/
+let butttolink1 = document.querySelector("#butttolink1");
+let butttolink2 = document.querySelector("#butttolink2");
+let butttolink3 = document.querySelector("#butttolink3");
+let butttolink4 = document.querySelector("#butttolink4");
+
+let buttarr = [butttolink1, butttolink2, butttolink3, butttolink4];
+
+buttarr.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    switch (e.target.id) {
+      case "butttolink1":
+        document
+          .querySelector("#section1")
+          .scrollIntoView({ block: "start", behavior: "smooth" });
+        break;
+      case "butttolink2":
+        document
+          .querySelector("#section2")
+          .scrollIntoView({ block: "start", behavior: "smooth" });
+        break;
+      case "butttolink3":
+        document
+          .querySelector("#section3")
+          .scrollIntoView({ block: "start", behavior: "smooth" });
+        break;
+      case "butttolink4":
+        document
+          .querySelector("#section4")
+          .scrollIntoView({ block: "start", behavior: "smooth" });
+        break;
+
+      default:
+        console.log(`Sorry, we are out of ${e.target.id}.`);
+        break;
+    }
+    event.preventDefault();
+  });
+});
